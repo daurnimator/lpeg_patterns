9e078af3ff542d21983bff58b9b26b9e99c31026
James Callahan <james@chatid.com>
2012-12-05 16:38:37 -0500

    lpeg_patterns: Update email pattern

--- lpeg_patterns.lua
+++ lpeg_patterns.lua
@@ -10,28 +10,49 @@ local R = lpeg.R
 local S = lpeg.S
 local V = lpeg.V
 local C = lpeg.C
+local Cs = lpeg.Cs
 
-local CHAR  = R"\0\127"
-local SPACE = S"\40\32"
+-- Core Rules https://tools.ietf.org/html/rfc5234#appendix-B.1
+local CHAR  = R"\1\127"
+local CRLF  = P"\r\n"
 local CTL   = R"\0\31" + P"\127"
+local VCHAR = R"\21\126"
+local WSP   = S" \t"
 
-local specials = S[=[()<>@,;:\".[]]=]
+local email do --https://tools.ietf.org/html/rfc5322#section-2.2.3
 
-local atom = (CHAR-specials-SPACE-CTL)^1
-local dtext = CHAR - S"[]\\\13"
-local qtext = CHAR - S'"\\\13'
-local quoted_pair = "\\" * CHAR
-local domain_literal = P"[" * ( dtext + quoted_pair )^0 + P"]"
-local quoted_string = P'"' * ( qtext + quoted_pair )^0 * P'"'
-local word = atom + quoted_string
+	local quoted_pair = Cs ( "\\" * C(VCHAR + WSP) / function(...) return ... end )
 
-local email do
-	-- Implements an email "addr-spec" according to RFC822
-	local domain_ref = atom
-	local sub_domain = domain_ref + domain_literal
-	local domain     = sub_domain * ( P"." * sub_domain )^0
-	local local_part = word * ( P"." * word )^0
-	local addr_spec  = local_part * P"@" * C(domain)
+	-- Folding White Space
+	local FWS = Cs ( (WSP^0 * CRLF)^-1 * WSP^1 / " " ) -- Fold whitespace into a single " "
+
+	-- Comments
+	local ctext   = R"\33\39" + R"\42\91" + R"\93\126"
+	local comment = P {
+		V"comment" ;
+		ccontent = ctext + quoted_pair + V"comment" ;
+		comment  = P"("* C ( (FWS^-1*V"ccontent")^0 ) * FWS^-1 * P")" ;
+	}
+	local CFWS = ((FWS^-1 * comment)^1 * FWS^-1 + FWS ) / function() end
+
+	-- Atom
+	local specials      = S[=[()<>@,;:\".[]]=]
+	local atext         = CHAR-specials-P" "-CTL
+	local atom          = CFWS^-1 * C(atext^1) * CFWS^-1
+	local dot_atom_text = atext^1 * ( P"." * atext^1 )^0
+	local dot_atom      = CFWS^-1 * C(dot_atom_text) * CFWS^-1
+
+	-- Quoted Strings
+	local qtext          = S"\33"+R("\35\91","\93\126")
+	local qcontent       = qtext + quoted_pair
+	local quoted_string  = CFWS^-1 * P'"' * Cs((FWS^-1 * qcontent)^0) * FWS^-1 * P'"' * CFWS^-1
+
+	-- Addr-spec
+	local dtext          = R("\33\90","\94\126")
+	local domain_literal = CFWS^-1 * P"[" * C((FWS^-1 * dtext)^0) * FWS^-1 * P"]" * CFWS^-1
+	local domain         = dot_atom + domain_literal
+	local local_part     = dot_atom + quoted_string
+	local addr_spec      = local_part * P"@" * domain
 
 	email = addr_spec
 end
