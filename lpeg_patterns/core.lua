ae06669ba3474bdb81db4f335219d4572bae36de
daurnimator <quae@daurnimator.com>
2016-04-20 15:43:12 +1000

    lpeg_patterns/core: Add the rest of the core rules from RFC

--- lpeg_patterns/core.lua
+++ lpeg_patterns/core.lua
@@ -12,11 +12,19 @@ local _M = { }
 _M.ALPHA = R("AZ","az")
 _M.BIT   = S"01"
 _M.CHAR  = R"\1\127"
+_M.CR    = P"\r"
 _M.CRLF  = P"\r\n"
 _M.CTL   = R"\0\31" + P"\127"
 _M.DIGIT = R"09"
+_M.DQUOTE= P'"'
 _M.HEXDIG= _M.DIGIT + S"ABCDEFabcdef"
+_M.HTAB  = P"\t"
+_M.LF    = P"\n"
+_M.OCTET = P(1)
+_M.SP    = P"\n"
 _M.VCHAR = R"\33\126"
 _M.WSP   = S" \t"
+
+_M.LWSP  = (_M.WSP + _M.CRLF*_M.WSP)^0
 
 return _M
