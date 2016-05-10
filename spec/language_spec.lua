76e5c5a46b7646babf5b57220b77c4680c66a08d
daurnimator <quae@daurnimator.com>
2016-05-10 16:40:50 +1000

    spec/language_spec: Restructure into many tests

--- spec/language_spec.lua
+++ spec/language_spec.lua
@@ -3,54 +3,66 @@ local EOF = lpeg.P(-1)
 describe("language tags", function()
 	local language = require "lpeg_patterns.language"
 	local langtag = lpeg.Ct(language.langtag) * EOF
-	it("parses examples from RFC 5646 Appendix A", function()
-		-- Simple language subtag:
-		assert.same({language = "de"; variant = {}; extension = {}}, langtag:match "de") -- German
-		assert.same({language = "fr"; variant = {}; extension = {}}, langtag:match "fr") -- French
-		assert.same({language = "ja"; variant = {}; extension = {}}, langtag:match "ja") -- Japanese
-		-- assert.same({language = "i-enochian"; variant = {}; extension = {}}, langtag:match "i-enochian") -- example of a grandfathered tag
-		-- Language subtag plus Script subtag:
-		assert.same({language = "zh"; script = "Hant"; variant = {}; extension = {}}, langtag:match "zh-Hant") -- Chinese written using the Traditional Chinese script
-		assert.same({language = "zh"; script = "Hans"; variant = {}; extension = {}}, langtag:match "zh-Hans") -- Chinese written using the Simplified Chinese script
-		assert.same({language = "sr"; script = "Cyrl"; variant = {}; extension = {}}, langtag:match "sr-Cyrl") -- Serbian written using the Cyrillic script
-		assert.same({language = "sr"; script = "Latn"; variant = {}; extension = {}}, langtag:match "sr-Latn") -- Serbian written using the Latin script
-		-- Extended language subtags and their primary language subtag counterparts:
-		assert.same({language = "zh"; extlang = "cmn", script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "zh-cmn-Hans-CN") -- Chinese, Mandarin, Simplified script, as used in China
-		assert.same({language = "cmn"; script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "cmn-Hans-CN") -- Mandarin Chinese, Simplified script, as used in China
-		assert.same({language = "zh"; extlang = "yue"; region = "HK"; variant = {}; extension = {}}, langtag:match "zh-yue-HK") -- Chinese, Cantonese, as used in Hong Kong SAR
-		assert.same({language = "yue"; region = "HK"; variant = {}; extension = {}}, langtag:match "yue-HK") -- Cantonese Chinese, as used in Hong Kong SAR
-		-- Language-Script-Region:
-		assert.same({language = "zh"; script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "zh-Hans-CN") -- Chinese written using the Simplified script as used in mainland China
-		assert.same({language = "sr"; script = "Latn"; region = "RS"; variant = {}; extension = {}}, langtag:match "sr-Latn-RS") -- Serbian written using the Latin script as used in Serbia
-		-- Language-Variant:
-		assert.same({language = "sl"; variant = {"rozaj"}; extension = {}}, langtag:match "sl-rozaj") -- Resian dialect of Slovenian
-		assert.same({language = "sl"; variant = {"rozaj", "biske"}; extension = {}}, langtag:match "sl-rozaj-biske") -- San Giorgio dialect of Resian dialect of Slovenian
-		assert.same({language = "sl"; variant = {"nedis"}; extension = {}}, langtag:match "sl-nedis") -- Nadiza dialect of Slovenian
-		-- Language-Region-Variant:
-		assert.same({language = "de"; region = "CH"; variant = {"1901"}; extension = {}}, langtag:match "de-CH-1901") -- German as used in Switzerland using the 1901 variant [orthography]
-		assert.same({language = "sl"; region = "IT"; variant = {"nedis"}; extension = {}}, langtag:match "sl-IT-nedis") -- Slovenian as used in Italy, Nadiza dialect
-		-- Language-Script-Region-Variant:
-		assert.same({language = "hy"; script = "Latn"; region = "IT"; variant = {"arevela"}; extension = {}}, langtag:match "hy-Latn-IT-arevela") -- Eastern Armenian written in Latin script, as used in Italy
-		-- Language-Region:
-		assert.same({language = "de"; region = "DE"; variant = {}; extension = {}}, langtag:match "de-DE") -- German for Germany
-		assert.same({language = "en"; region = "US"; variant = {}; extension = {}}, langtag:match "en-US") -- English as used in the United States
-		assert.same({language = "es"; region = "419"; variant = {}; extension = {}}, langtag:match "es-419") -- Spanish appropriate for the Latin America and Caribbean region using the UN region code
-		-- Private use subtags:
-		assert.same({language = "de"; region = "CH"; privateuse = {"phonebk"}; variant = {}; extension = {}}, langtag:match "de-CH-x-phonebk")
-		assert.same({language = "az"; script = "Arab"; privateuse = {"AZE", "derbend"}; variant = {}; extension = {}}, langtag:match "az-Arab-x-AZE-derbend")
-		-- Private use registry values:
-		-- assert.same({language = nil; privateuse = {"whatever"}; variant = {}; extension = {}}, langtag:match "x-whatever") -- private use using the singleton 'x'
-		assert.same({language = "qaa"; script = "Qaaa"; region = "QM"; privateuse = {"southern"}; variant = {}; extension = {}}, langtag:match "qaa-Qaaa-QM-x-southern") -- all private tags
-		assert.same({language = "de"; script = "Qaaa"; variant = {}; extension = {}}, langtag:match "de-Qaaa") -- German, with a private script
-		assert.same({language = "sr"; script = "Latn"; region = "QM"; variant = {}; extension = {}}, langtag:match "sr-Latn-QM") -- Serbian, Latin script, private region
-		assert.same({language = "sr"; script = "Qaaa"; region = "RS"; variant = {}; extension = {}}, langtag:match "sr-Qaaa-RS") -- Serbian, private script, for Serbia
-		-- Tags that use extensions (examples ONLY -- extensions MUST be defined by revision or update to this document, or by RFC):
-		assert.same({language = "en"; region = "US"; variant = {}; extension = { u = {"islamcal"}}}, langtag:match "en-US-u-islamcal")
-		assert.same({language = "zh"; region = "CN"; variant = {}; extension = { a = {"myext"}}; privateuse = {"private"}}, langtag:match "zh-CN-a-myext-x-private")
-		assert.same({language = "en"; variant = {}; extension = { a = {"myext"}, b = {"another"}}}, langtag:match "en-a-myext-b-another")
-		-- Some Invalid Tags:
-		assert.falsy(langtag:match "de-419-DE") -- two region tags
-		assert.falsy(langtag:match "a-DE") -- use of a single-character subtag in primary position; note that there are a few grandfathered tags that start with "i-" that are valid
-		-- assert.falsy(langtag:match "ar-a-aaa-b-bbb-a-ccc") -- two extensions with same single-letter prefix
+	describe("examples from RFC 5646 Appendix A", function()
+		it("Parses Simple language subtag", function()
+			assert.same({language = "de"; variant = {}; extension = {}}, langtag:match "de") -- German
+			assert.same({language = "fr"; variant = {}; extension = {}}, langtag:match "fr") -- French
+			assert.same({language = "ja"; variant = {}; extension = {}}, langtag:match "ja") -- Japanese
+			-- assert.same({language = "i-enochian"; variant = {}; extension = {}}, langtag:match "i-enochian") -- example of a grandfathered tag
+		end)
+		it("Parses Language subtag plus Script subtag", function()
+			assert.same({language = "zh"; script = "Hant"; variant = {}; extension = {}}, langtag:match "zh-Hant") -- Chinese written using the Traditional Chinese script
+			assert.same({language = "zh"; script = "Hans"; variant = {}; extension = {}}, langtag:match "zh-Hans") -- Chinese written using the Simplified Chinese script
+			assert.same({language = "sr"; script = "Cyrl"; variant = {}; extension = {}}, langtag:match "sr-Cyrl") -- Serbian written using the Cyrillic script
+			assert.same({language = "sr"; script = "Latn"; variant = {}; extension = {}}, langtag:match "sr-Latn") -- Serbian written using the Latin script
+		end)
+		it("Parses Extended language subtags and their primary language subtag counterparts", function()
+			assert.same({language = "zh"; extlang = "cmn", script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "zh-cmn-Hans-CN") -- Chinese, Mandarin, Simplified script, as used in China
+			assert.same({language = "cmn"; script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "cmn-Hans-CN") -- Mandarin Chinese, Simplified script, as used in China
+			assert.same({language = "zh"; extlang = "yue"; region = "HK"; variant = {}; extension = {}}, langtag:match "zh-yue-HK") -- Chinese, Cantonese, as used in Hong Kong SAR
+			assert.same({language = "yue"; region = "HK"; variant = {}; extension = {}}, langtag:match "yue-HK") -- Cantonese Chinese, as used in Hong Kong SAR
+		end)
+		it("Parses Language-Script-Region", function()
+			assert.same({language = "zh"; script = "Hans"; region = "CN"; variant = {}; extension = {}}, langtag:match "zh-Hans-CN") -- Chinese written using the Simplified script as used in mainland China
+			assert.same({language = "sr"; script = "Latn"; region = "RS"; variant = {}; extension = {}}, langtag:match "sr-Latn-RS") -- Serbian written using the Latin script as used in Serbia
+		end)
+		it("Parses Language-Variant", function()
+			assert.same({language = "sl"; variant = {"rozaj"}; extension = {}}, langtag:match "sl-rozaj") -- Resian dialect of Slovenian
+			assert.same({language = "sl"; variant = {"rozaj", "biske"}; extension = {}}, langtag:match "sl-rozaj-biske") -- San Giorgio dialect of Resian dialect of Slovenian
+			assert.same({language = "sl"; variant = {"nedis"}; extension = {}}, langtag:match "sl-nedis") -- Nadiza dialect of Slovenian
+		end)
+		it("Parses Language-Region-Variant", function()
+			assert.same({language = "de"; region = "CH"; variant = {"1901"}; extension = {}}, langtag:match "de-CH-1901") -- German as used in Switzerland using the 1901 variant [orthography]
+			assert.same({language = "sl"; region = "IT"; variant = {"nedis"}; extension = {}}, langtag:match "sl-IT-nedis") -- Slovenian as used in Italy, Nadiza dialect
+		end)
+		it("Parses Language-Script-Region-Variant", function()
+			assert.same({language = "hy"; script = "Latn"; region = "IT"; variant = {"arevela"}; extension = {}}, langtag:match "hy-Latn-IT-arevela") -- Eastern Armenian written in Latin script, as used in Italy
+		end)
+		it("Parses Language-Region", function()
+			assert.same({language = "de"; region = "DE"; variant = {}; extension = {}}, langtag:match "de-DE") -- German for Germany
+			assert.same({language = "en"; region = "US"; variant = {}; extension = {}}, langtag:match "en-US") -- English as used in the United States
+			assert.same({language = "es"; region = "419"; variant = {}; extension = {}}, langtag:match "es-419") -- Spanish appropriate for the Latin America and Caribbean region using the UN region code
+		end)
+		it("Parses private use subtags", function()
+			assert.same({language = "de"; region = "CH"; privateuse = {"phonebk"}; variant = {}; extension = {}}, langtag:match "de-CH-x-phonebk")
+			assert.same({language = "az"; script = "Arab"; privateuse = {"AZE", "derbend"}; variant = {}; extension = {}}, langtag:match "az-Arab-x-AZE-derbend")
+		end)
+		it("Parses private use registry values", function()
+			-- assert.same({language = nil; privateuse = {"whatever"}; variant = {}; extension = {}}, langtag:match "x-whatever") -- private use using the singleton 'x'
+			assert.same({language = "qaa"; script = "Qaaa"; region = "QM"; privateuse = {"southern"}; variant = {}; extension = {}}, langtag:match "qaa-Qaaa-QM-x-southern") -- all private tags
+			assert.same({language = "de"; script = "Qaaa"; variant = {}; extension = {}}, langtag:match "de-Qaaa") -- German, with a private script
+			assert.same({language = "sr"; script = "Latn"; region = "QM"; variant = {}; extension = {}}, langtag:match "sr-Latn-QM") -- Serbian, Latin script, private region
+			assert.same({language = "sr"; script = "Qaaa"; region = "RS"; variant = {}; extension = {}}, langtag:match "sr-Qaaa-RS") -- Serbian, private script, for Serbia
+		end)
+		it("Parses tags that use extensions", function()
+			assert.same({language = "en"; region = "US"; variant = {}; extension = { u = {"islamcal"}}}, langtag:match "en-US-u-islamcal")
+			assert.same({language = "zh"; region = "CN"; variant = {}; extension = { a = {"myext"}}; privateuse = {"private"}}, langtag:match "zh-CN-a-myext-x-private")
+			assert.same({language = "en"; variant = {}; extension = { a = {"myext"}, b = {"another"}}}, langtag:match "en-a-myext-b-another")
+		end)
+		it("Rejects Invalid Tags", function()
+			assert.falsy(langtag:match "de-419-DE") -- two region tags
+			assert.falsy(langtag:match "a-DE") -- use of a single-character subtag in primary position; note that there are a few grandfathered tags that start with "i-" that are valid
+			-- assert.falsy(langtag:match "ar-a-aaa-b-bbb-a-ccc") -- two extensions with same single-letter prefix
+		end)
 	end)
 end)
