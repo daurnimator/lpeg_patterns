2ffd00b9051207d676117a7d767e637a7541086d
daurnimator <quae@daurnimator.com>
2018-07-15 20:08:11 +1000

    lpeg_patterns/http.lua: Public_Key_Pins validation
      - pin directives should be broken down by type
      - duplicates in non-pin directives aren't allowed
      - max-age is compulsory for non-report header

--- lpeg_patterns/http.lua
+++ lpeg_patterns/http.lua
@@ -592,8 +592,48 @@ _M.Forwarded = comma_sep_trim(forwarded_element)
 
 -- RFC 7469
 local Public_Key_Directives = directive * (_M.OWS * P";" * _M.OWS * directive)^0
-_M.Public_Key_Pins = Public_Key_Directives
-_M.Public_Key_Pins_Report_Only = Public_Key_Directives
+do
+	local function pkp_cmt(pins, t, k, v, ...)
+		-- duplicates are allowed if the directive name starts with "pin-"
+		local pin_name = k:match("^pin%-(.+)")
+		if pin_name then
+			local hashes = pins[pin_name]
+			if hashes then
+				hashes[#hashes+1] = v
+			else
+				hashes = {v}
+				pins[pin_name] = hashes
+			end
+		else
+			local old = t[k]
+			if old then
+				return false
+			end
+			t[k] = v
+		end
+		if ... then
+			return pkp_cmt(pins, t, ...)
+		else
+			return true
+		end
+	end
+	_M.Public_Key_Pins = Cmt(Public_Key_Directives, function(_, _, ...)
+		local pins = {}
+		local t = {}
+		local ok = pkp_cmt(pins, t, ...)
+		if ok and t["max-age"] then
+			return true, pins, t
+		end
+	end)
+	_M.Public_Key_Pins_Report_Only = Cmt(Public_Key_Directives, function(_, _, ...)
+		local pins = {}
+		local t = {}
+		local ok = pkp_cmt(pins, t, ...)
+		if ok then
+			return true, pins, t
+		end
+	end)
+end
 
 -- RFC 7486
 _M.Hobareg = C"regok" + C"reginwork"
--- spec/http_spec.lua
+++ spec/http_spec.lua
@@ -277,14 +277,43 @@ describe("http patterns", function()
 	end)
 	it("Parses a HPKP header", function()
 		-- Example from RFC 7469 2.1.5
-		local pkp_patt = lpeg.Cf(
-			lpeg.Ct(true) * http.Public_Key_Pins,
-			function(t, k, v) table.insert(t, {k,v}) return t end
-		) * EOF
+		local pkp_patt = lpeg.Ct(http.Public_Key_Pins) * EOF
 		assert.same({
-			{ "max-age", "3000" };
-			{ "pin-sha256", "d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM=" };
-			{ "pin-sha256", "E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g=" };
+			{
+				sha256 = {
+					"d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM=";
+					"E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g=";
+				};
+			}, {
+				["max-age"] = "3000";
+			}
 		}, pkp_patt:match([[max-age=3000; pin-sha256="d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM="; pin-sha256="E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g="]]))
+
+		-- max-age is compulsory
+		assert.same(nil, pkp_patt:match([[pin-sha256="d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM="; pin-sha256="E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g="]]))
+	end)
+	it("Parses a HPKP Report header", function()
+		-- Example from RFC 7469 2.1.5
+		local pkp_patt = lpeg.Ct(http.Public_Key_Pins_Report_Only) * EOF
+		assert.same({
+			{
+				sha256 = {
+					"d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM=";
+					"E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g=";
+				};
+			}, {
+				["max-age"] = "3000";
+			}
+		}, pkp_patt:match([[max-age=3000; pin-sha256="d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM="; pin-sha256="E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g="]]))
+		-- max-age isn't compulsory
+		assert.same({
+			{
+				sha256 = {
+					"d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM=";
+					"E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g=";
+				};
+			}, {
+			}
+		}, pkp_patt:match([[pin-sha256="d6qzRu9zOECb90Uez27xWltNsj0e1Md7GkYYkVoZWmM="; pin-sha256="E9CZ9INDbd+2eRQozYqqbQ2yXLVKB9+xcprMF+44U1g="]]))
 	end)
 end)
